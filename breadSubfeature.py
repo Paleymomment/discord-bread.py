2a83672277ee067119a1a0299ed0f2eda8bc5b66
Paleymomment <78581849+Paleymomment@users.noreply.github.com>
2021-12-29 22:48:49 +0200

    Update breadSubfeature.py

--- breadSubfeature.py
+++ breadSubfeature.py
@@ -1,2 +1,14 @@
 import discord
 import random
+from discord_webhook import DiscordWebhook, DiscordEmbed
+
+
+client = discord.Client(self_bot=True)
+
+@client.event
+async def on_ready():
+    print('ready')
+    
+    # This can always be changed if you download the python file directly
+    if message.content.startswith('spiderman. give me bread.'):
+      
